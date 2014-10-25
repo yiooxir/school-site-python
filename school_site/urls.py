9e55183128216819cfa7f4635fa81bce81aadea3
sergey <spectrs@bk.ru>
2014-10-25 13:47:18 +0400

    fix

---
+++ school_site/urls.py
@@ -0,0 +1,29 @@
+from django.conf.urls import patterns, include, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
+import settings
+
+
+from django.contrib import admin
+admin.autodiscover()
+
+
+urlpatterns = static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += staticfiles_urlpatterns()
+
+urlpatterns += patterns('',
+                        url(r'^admin_tools/', include('admin_tools.urls')),
+                        url(r'^admin/', include(admin.site.urls)),
+                        )
+
+urlpatterns += patterns('',
+                        (r'^tinymce/', include('tinymce.urls')),
+                        (r'^admin/filebrowser/', include('filebrowser.urls')),
+                        )
+
+urlpatterns += patterns('',
+                        url(r'^$', 'content.main_page.view.index'),
+                        url(r'^journal/', include('content.journal.url')),
+                        url(r'^photo/', include('content.photo_gallery.url')),
+
+                        )
